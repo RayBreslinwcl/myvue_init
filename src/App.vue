203b172ff2d55921f65894e073786705e5726aa0
ray <2728928100@qq.com>
2022-10-03 21:56:38 +0800

    vue——echarts

--- src/App.vue
+++ src/App.vue
@@ -6,23 +6,33 @@
 
     <router-link to="/page1">Page1</router-link>
     <router-link to="/page2">Page2</router-link>
-    
+
     <div>
       <router-link to="/user/xianyu">动态路由咸鱼</router-link>
       <router-link to="/user/mengxiang">动态路由梦想</router-link>
 
       <router-link to="/vcharts">vcharts</router-link>
     </div>
+
+     <elementui></elementui>
+
+
   </div>
-
   <!-- //添加两个router-link标签 -->
-
-
+ 
 </template>
 
+
+
 <script>
+
+import Elementui from './components/elementui.vue'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Elementui
+  }
 }
 </script>
 
@@ -35,4 +45,34 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.el-row {
+  margin-bottom: 200px;
+  /* &:last-child {
+    margin-bottom: 0;
+  } */
+}
+.el-col {
+  border-radius: 6px;
+  
+}
+.bg-purple-dark {
+  background: #99a9bf;
+}
+.bg-purple {
+  background: #d3dce6;
+}
+.bg-purple-light {
+  background: #e5e9f2;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+.row-bg {
+  padding: 10px 0;
+  background-color: #f9fafc;
+}
 </style>
+
+
